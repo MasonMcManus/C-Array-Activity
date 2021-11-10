0567aac75af3a724144f03ce409dfe090b89bf02
MasonMcManus <MCMANUS2@tritons.iowacentral.edu>
2021-11-10 11:53:43 -0600

    Add project files.

---
+++ Main.cpp
@@ -0,0 +1,46 @@
+// Create an array of 100 integers, sort the random numbers so the even numbers will be above the odd numbers, from smallest to largest, while also getting put back into the original array.
+#include <iostream>
+#include <fstream>
+#include <ctime>
+#include <cmath>
+
+using namespace std;
+
+enum order { Odd, Even };
+
+int main()
+{
+	int rand[100]
+	int x
+		for (int = 0; int <100; i++)
+		{
+			order a = even;
+			cout << a << endl;
+
+		}
+	if (a % 2 != 0)
+		break;
+	else if (x % 2 != 0)
+	{
+		order a = odd;
+		cout << a << endl;
+	}
+	break;
+}
+
+do
+{
+	cout << a << endl;
+	cin.get(even);
+	{
+		while (a = even);
+	}
+	do
+	{
+		cout << a << endl;
+		cin.get(odd);
+	} 
+	while (a = odd);
+
+	return 0;
+}
